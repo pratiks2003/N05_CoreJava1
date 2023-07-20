e8ac63b697b26bb050d1fbeb16fd8f5fca34b44c
pratik shinde <pratikshinde1239@gmail.com>
2023-07-20 12:05:00 +0530

    first commit - core java - user input

---
+++ dayone/src/met/tns/intro/FirstTechDay.java
@@ -0,0 +1,34 @@
+package met.tns.intro;
+import java.util.*;
+public class FirstTechDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// to take user input
+		
+		System.out.println("enter your number");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num);
+	
+
+		System.out.println("enter your float number");
+		Scanner sc = new Scanner(System.in);
+		float num1 = sc.nextFloat();
+		System.out.println(num1);
+		
+
+		System.out.println("enter your char");
+		Scanner sc = new Scanner(System.in);
+		char num3 = sc.next().charAt(0);
+		System.out.println(num3);
+	
+		
+		System.out.println("enter your string");
+		Scanner sc = new Scanner(System.in);
+		String num4 = sc.next();
+		System.out.println(num4);
+	}
+	
+
+}
