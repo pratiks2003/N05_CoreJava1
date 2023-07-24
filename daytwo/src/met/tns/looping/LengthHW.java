7b7afc396fc8396612fa050cd67e7edf4ec21e79
pratik shinde <pratikshinde1239@gmail.com>
2023-07-24 22:50:31 +0530

    day two - Homework - second commit

---
+++ daytwo/src/met/tns/looping/LengthHW.java
@@ -0,0 +1,15 @@
+package met.tns.looping;
+import java.util.*;
+public class LengthHW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		String c = String.valueOf(a);
+		int b = c.length();
+		System.out.println("number of digits in "+a+" is "+b);
+	}
+
+}
---
+++ daytwo/src/met/tns/looping/TriangleHW.java
@@ -0,0 +1,20 @@
+package met.tns.looping;
+import java.util.*;
+public class TriangleHW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i=1;i<a;i++) {
+			
+			for(int j=1;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("1");
+			
+		}
+	}
+
+}
